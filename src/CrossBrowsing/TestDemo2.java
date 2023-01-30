7eafd8c8a7d68da91b4f1da42060c74ffaeef5f3
dpksk99 <dpksk99@gmail.com>
2023-01-30 12:56:12 +0530

    first commit

---
+++ src/CrossBrowsing/TestDemo2.java
@@ -0,0 +1,27 @@
+package CrossBrowsing;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.Assert;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+public class TestDemo2 
+{
+	@Test
+	public void titleCheck() throws MalformedURLException
+	{
+	
+		MutableCapabilities caps = new MutableCapabilities();
+		
+		//WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.8:4444"), caps);
+		WebDriver driver = new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"), caps);
+		
+		driver.get("https://www.youtube.com/");
+		Assert.assertTrue(driver.getTitle().matches("YouTube"));
+		driver.close();
+	}
+}
